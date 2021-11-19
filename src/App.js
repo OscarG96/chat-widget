f67a0490c2f512bd2852c3345453dc90d92cd693
Oscar G <oscar.gtzv@hotmail.com>
2021-11-18 23:47:08 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { FaRegPaperPlane } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div style={{position: 'fixed',
+      bottom: '20px',
+      right: '10px'}} className="App">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Button className="rounded-circle" variant="primary"><FaRegPaperPlane /></Button>{' '}
     </div>
   );
 }
