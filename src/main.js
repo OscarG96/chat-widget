425a8b0fc667665d65bb25371b3678744f5ceea1
Oscar G <oscar.gtzv@hotmail.com>
2021-11-24 23:57:31 -0600

    added modules to use html

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
+import btn from './button/button.html'
 function app() {
     console.log('JS-Widget starting');
-    var btn = document.createElement("BUTTON");   // Create a <button> element
-    btn.innerHTML = "CLICK ME";                   // Insert text
-    document.body.appendChild(btn);    
+    // var btn = document.createElement("BUTTON");   // Create a <button> element
+    document.body.appendChild(btn);
+    // btn.innerHTML = 'Click me!';
+    // var img = document.createElement("img")
+    // img.src = "https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png"
+    // img.alt = "charla"
+    // document.body.appendChild(btn);    
+    // document.body.appendChild(img)
 }
 
 app();
+
+
+// cellphones css
+// background-color: blue;
+//     color: white;
+//     content: &&;
+//     display: block;
+//     height: 2rem;
+//     width: 2rem;
+//     border-radius: 50%;
+//     border: 1px solid red;
+// src/assets/message-svgrepo-com.svg
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/main.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, './docs'),
-    filename: 'index_bundle.js',
-  },
-  plugins: [new HtmlWebpackPlugin()],
+module.exports = (env) => {
+  const isDevBuild = !(env && env.prod);
+  return [{
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, './docs'),
+      filename: 'index_bundle.js',
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    
+    module: { 
+      rules: [
+        { test: /\.html$/i, use: 'html-loader' },
+        { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')]}
+      ]
+     }
+  }]
 };
