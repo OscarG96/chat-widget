08d013a21d7f41b1302bc92b3f92ff50b6a1c64c
Oscar G <oscar.gtzv@hotmail.com>
2021-12-07 00:14:06 -0600

    made changes to widget css

--- src/main.js
+++ src/main.js
@@ -1,29 +1,51 @@
+
+import chatWindow from './ChatWindow/chatwindow.html';
+import './ChatWindow/chatwindow.css'
 import btn from './button/button.html'
-import './button/button.css'
-function app() {
-    console.log('JS-Widget starting');
-    console.log('button', typeof btn);
-    // var btn = document.createElement("BUTTON");   // Create a <button> element
-    let content = document.createElement('div')
-    content.innerHTML = btn
-    content.className = 'chat-widget'
-    document.body.appendChild(content);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    function app() {
+        const chatBtn = document.querySelector('.chat-btn');
+        const popup = document.querySelector('.chat-popup');
     
-    document.getElementById('myBtn').onclick = function(e){
-        alert('click');
+        console.log('JS-Widget starting');
+    
+        let content = document.createElement('div')
+        content.innerHTML = `
+            ${chatWindow}
+            ${btn}
+        `
+        document.body.appendChild(content);
+        console.log(chatBtn)
+        // btn.addEventListener('click', ()=>{
+        //     console.log('btn clicked')
+        //     popup.classList.toggle('show');
+        // })
+        document.getElementById('btnToggleWidget').onclick = function(e){
+            console.log('btn clicked')
+            // popup.classList.toggle('show');
+            document.getElementsByClassName('chat-popup').className += 'show'
+            
+        }
+        
+        // send msg 
+        // submitBtn.addEventListener('click', ()=>{
+        //     let userInput = inputElm.value;
+        
+        //     let temp = `<div class="out-msg">
+        //     <span class="my-msg">${userInput}</span>
+        //     <img src="img/me.jpg" class="avatar">
+        //     </div>`;
+        
+        //     chatArea.insertAdjacentHTML("beforeend", temp);
+        //     inputElm.value = '';
+        
+        // })
     }
-}
+    
+    app();
+});
 
-app();
 
 
-// cellphones css
-// background-color: blue;
-//     color: white;
-//     content: &&;
-//     display: block;
-//     height: 2rem;
-//     width: 2rem;
-//     border-radius: 50%;
-//     border: 1px solid red;
-// src/assets/message-svgrepo-com.svg
