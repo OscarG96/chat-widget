bda1b067a58caddf499cc374e766746523b48319
Oscar G <oscar.gtzv@hotmail.com>
2021-12-19 23:51:03 -0600

    changed url for user create

--- src/utils/http.service.js
+++ src/utils/http.service.js
@@ -3,9 +3,9 @@ const URL = "http://localhost:3000"
 
 
 export function addNewUser(username, useremail) {
-    axios.post(`${URL}/createuser`, {
-        username, 
-        useremail
+    axios.post(`${URL}/user/createuser`, {
+        name: username, 
+        email: useremail
     }).then(() => console.log('user created'), (message) => console.warn('something failed', message))
     .catch(error => console.error(error))
 }
